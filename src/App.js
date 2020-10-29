4530fd1483f4e1e323198493caeadef34d34dbf6
Louis Atkinson <atkinsonlouis@gmail.com>
2020-10-29 14:10:38 +0200

    Added buttons allowing total to be changes, Also prevented the remove one button from allowing the total to be less than 0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    likes: 0,
+  };
+
+  addHandler = () => {
+    this.setState({
+      likes: this.state.likes + 1,
+    });
+  };
+
+  removeHandler = () => {
+    if (this.state.likes > 0) {
+      this.setState({
+        likes: this.state.likes - 1,
+      });
+    } else {
+      this.setState({
+        likes: 0,
+      });
+    }
+  };
+
+  resetHandler = () => {
+    this.setState({
+      likes: 0,
+    });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Total Likes: {this.state.likes}</h1>
+        <button onClick={this.addHandler}>Add One</button>
+        <button onClick={this.removeHandler}>Remove One</button>
+        <button onClick={this.resetHandler}>Reset</button>
+      </div>
+    );
   }
 }
 
